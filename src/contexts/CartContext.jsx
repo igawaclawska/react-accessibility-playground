3b2d9577fabbaa87956daa8106477ed76b53d1e4
iga waclawska <iga.waclawska@gmail.com>
2025-02-01 23:42:08 +0100

    add local storage support

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,9 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem("cart");
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  console.log("CartProvider: cart", cart);
 
   const addToCart = (product, quantity) => {
     setCart((prevCart) => {
