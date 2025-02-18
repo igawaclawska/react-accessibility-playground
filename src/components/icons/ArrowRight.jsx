3673214477d164b7c2801f8245c2b27806d486da
iga waclawska <iga.waclawska@gmail.com>
2025-02-18 21:27:42 +0100

    add ariaHidden prop to the svg components

--- src/components/icons/ArrowRight.jsx
+++ src/components/icons/ArrowRight.jsx
@@ -1,6 +1,7 @@
-const ArrowRight = () => {
+const ArrowRight = ({ ariaHidden }) => {
   return (
     <svg
+      aria-hidden={ariaHidden}
       xmlns="http://www.w3.org/2000/svg"
       height="24px"
       viewBox="0 0 24 24"
